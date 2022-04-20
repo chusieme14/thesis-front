aff923c7bfecf9b4a33f3c53809f2136b9a367b9
michael <jmpmagistrado@gmail.com>
2022-04-20 20:45:33 +0800

    home

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,6 +1,18 @@
 <template>
-    <div>
-        headers
+    <div class="header">
+        <div class="inner-header">
+            <div class="logo">
+                header
+            </div>
+            <div class="search">
+                <input placeholder="Search">
+            </div>
+            <div class="header-icons text-right">
+                <v-icon>mdi-home</v-icon>
+                <v-icon>mdi-heart-outline</v-icon>
+                <v-icon>mdi-account-circle</v-icon>
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -8,3 +20,71 @@ export default {
     
 }
 </script>
+
+<style>
+    .header {
+        height: 60px;
+        position: fixed;
+        left: 0;
+        right: 0;
+        top: 0;
+        background: #fff;
+        z-index: 99;
+        border-bottom: 1px solid rgba(var(--b6a,219,219,219),1);
+    }
+
+    .inner-header {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        max-width: 975px;
+        padding: 0 20px;
+        width: 100%;
+        z-index: 10;
+        margin: auto;
+        height: 60px;
+        transition: height .2s ease-in-out;
+    }
+
+    .search input {
+        -webkit-box-align: center;
+        -webkit-align-items: center;
+        -ms-flex-align: center;
+        align-items: center;
+        display: -webkit-box;
+        display: -webkit-flex;
+        display: -ms-flexbox;
+        display: flex;
+        -webkit-box-flex: 0;
+        -webkit-flex: 0 1 auto;
+        -ms-flex: 0 1 auto;
+        flex: 0 1 auto;
+        height: 36px;
+        min-width: 125px;
+        width: 268px;
+        background: #efefef;
+        border-radius: 8px;
+        padding: 0 20px;
+    }
+
+    .search input:focus-visible {
+        outline: none;
+    }
+
+    .search {
+        -webkit-box-flex: 1;
+        flex: 0 1 auto;
+    }
+
+    .logo {
+        -webkit-box-flex: 1;
+        flex: 1 0 127px;
+    }
+
+    .header-icons {
+        flex: 1 0 127px;
+        -webkit-box-flex: 1;
+        -webkit-flex: 1 0 127px;
+        -ms-flex: 1 0 127px;
+    }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,65 @@
 <template>
-  <v-row justify="center" align="center">
+  <v-row justify="center" class="newsfeed">
     <v-col cols="12" sm="8" md="6">
       <v-card>
-        landing page
+        <div class="announcements"> 
+          <div class="my-day">
+            <div class="myday-one">
+                <v-avatar
+                  color="primary"
+                  size="56"
+                > <img src="https://mydubplate.checkmy.dev/wp-content/uploads/2022/04/disque-vinyle-personnalise-unite-1.png"></v-avatar>
+        
+                <p>test</p>
+            </div>
+          </div>
+
+          <div class="post">
+            <div>
+              <img src="/sample.jpg">
+            </div>
+          </div>
+
+        </div>
       </v-card>
     </v-col>
   </v-row>
 </template>
+
+
+<style>
+
+    .newsfeed {
+      margin-top: 45px;
+    }
+
+    .announcements {
+      padding-top: 30px;
+      margin-right: 28px;
+      max-width: 614px;
+      width: 100%;
+      background: transparent;
+      width: 100%;
+    }
+
+    .my-day {
+      background: #fff;
+      padding-left: 15px;
+      border: 1px solid rgba(var(--b6a,219,219,219),1);
+    }
+
+    .myday-one {
+      width: 70px;
+      text-align: center;
+    background: blue;
+    }
+
+    .post img {
+      margin-top: 30px;
+      width: 100%;
+    }
+
+</style>
 
 <script>
 export default {
